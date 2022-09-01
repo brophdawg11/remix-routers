6e613d43ee4b5fa13655363e553b904ee2507c99
Matt Brophy <matt@brophy.org>
2022-08-31 22:24:55 -0400

    again

--- packages/react-stub/reference-app/main.tsx
+++ packages/react-stub/reference-app/main.tsx
@@ -1,8 +1,8 @@
 import * as React from "react";
-// @ts-expect-error
 import * as ReactDOM from "react-dom/client";
 import App from "~/App";
 
+// @ts-expect-error
 ReactDOM.createRoot(document.getElementById("app")).render(
   <React.StrictMode>
     <App />
